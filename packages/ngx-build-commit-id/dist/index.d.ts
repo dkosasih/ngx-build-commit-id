27a471928b1c549d4a7fb13b457d916fe3d1318b
Deddy Kosasih <deddy.kosasih@carsales.office>
2019-12-16 11:35:21 +1100

    initial commit

---
+++ packages/ngx-build-commit-id/dist/index.d.ts
@@ -0,0 +1,5 @@
+import { json } from '@angular-devkit/core';
+export interface CommitBuilderSchema {
+}
+declare const _default: import("@angular-devkit/architect/src/internal").Builder<json.JsonObject & CommitBuilderSchema>;
+export default _default;
